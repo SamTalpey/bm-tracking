b68f68ae0b34825fe3c95bd6966cf80e09ea7fb9
Sam Talpey <sjtalpey@gmail.com>
2020-04-12 15:49:56 -0400

    Initial framework and webpage

---
+++ public/js/obserview.js
@@ -0,0 +1,26 @@
+// JS file for observation sheet/exit interviews
+
+// Generate page
+window.onload = function() {
+  console.log('Initializing page');
+};
+
+/**
+ * Function to initialize observation sheet
+ * Prompts for non-timestamped data 
+ */
+const initSheet = function() {
+  // Display boxes allowing for input of demographic, size, and other notes
+  // Buttons should be clear, submit, live observations, and interview
+};
+
+/**
+ * Function to toggle the type of observations on the sheet
+ * Call with true for live observations, and false for false observations
+ */
+const toggleSheet = function(isLive) {
+  // Update button text and onclick
+  // Clear obserview-sheet
+  // Fill in with correct format
+  //  Reference init observations if relevant
+};
