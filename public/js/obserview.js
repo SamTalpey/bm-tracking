69dfc974c0f489a5bb5b9850f8c1b7ca39466ee5
Sam Talpey <sjtalpey@gmail.com>
2020-04-13 17:56:56 -0400

    Need list of interview questions. base functionality almost done

--- public/js/obserview.js
+++ public/js/obserview.js
@@ -11,36 +11,15 @@
 // Generate page
 window.onload = function() {
   console.log('Initializing page');
-  initSheet();
+  toggleSheet(0);
 };
 
 /**
  * Function to initialize observation sheet
- * Prompts for non-timestamped data 
+ * May not be necessary due to toggleSheet(isLive)
  */
 const initSheet = function() {
-  console.log('Initializing obserview-sheet');
 
-  // Creating init observation elements
-  // TODO: Make size of boxes larger
-  let demographic = document.createElement('input');
-    demographic.id = 'obserview-sheet-demographic';
-  let size = document.createElement('input');
-    size.id = 'obserview-sheet-size';
-  let other = document.createElement('input');
-    other.id = 'obserview-sheet-other';
-
-  // Appending init observation elements
-  let observiewSheet = document.getElementById('obserview-sheet');
-  observiewSheet.innerHTML = 'Demographic: ';
-  observiewSheet.append(demographic);
-  observiewSheet.innerHTML += '<br><br>Size: ';
-  observiewSheet.append(size);
-  observiewSheet.innerHTML += '<br><br>Other Notes: ';
-  observiewSheet.append(other);
-
-  // Display boxes allowing for input of demographic, size, and other notes
-  // Buttons should be clear, submit, live observations, and interview
 };
 
 /**
@@ -48,15 +27,68 @@ const initSheet = function() {
  * Call with true for live observations, and false for init observations
  */
 const toggleSheet = function(isLive) {
-  // Update button text and onclick
-  // Clear obserview-sheet
-  // Fill in with correct format
-  //  Reference init observations if relevant
+  console.log('Toggling sheet');
+  let observiewSheet = document.getElementById('obserview-sheet');
+  observiewSheet.innerHTML = '';
+  let clearBtn = document.getElementById('obserview-clearbtn');
+  let submitBtn = document.getElementById('obserview-submitbtn');
+  let observationsBtn = document.getElementById('obserview-observationsbtn');
+
+  // Setting up live observation form
+  if(isLive) {
+    // Create clock/stopwatch
+    // TODO
+
+    // Create element to show current exhibit
+    // TODO
+
+    // Creating live observation submission form 
+    // TODO make size of text box larger
+    let liveInput = document.createElement('input');
+    liveInput.id = 'obserview-liveInput';
+
+    // Appending elements to sheet
+    observiewSheet.innerHTML += 'Observation Notes: ';
+    observiewSheet.append(liveInput);
+
+    // Editing button onclicks/text
+    // clearBtn.onclick = '';
+    // submitBtn.onclick = '';
+    // observationsBtn.onclick = 'toggleSheet(0)';
+
+  }
+  // Setting up initial observation form
+  else {
+    // Creating init observation elements
+    // TODO: Make size of boxes larger
+    let demographic = document.createElement('input');
+    demographic.id = 'obserview-sheet-demographic';
+    let size = document.createElement('input');
+    size.id = 'obserview-sheet-size';
+    let other = document.createElement('input');
+    other.id = 'obserview-sheet-other';
+
+    // Appending init observation elements
+    observiewSheet.innerHTML = 'Demographic: ';
+    observiewSheet.append(demographic);
+    observiewSheet.innerHTML += '<br><br>Size: ';
+    observiewSheet.append(size);
+    observiewSheet.innerHTML += '<br><br>Other Notes: ';
+    observiewSheet.append(other);
+
+    // Editing button onclicks/text
+    console.log('changing buttons')
+    // clearBtn.onclick = '';
+    // submitBtn.onclick = '';
+    observationsBtn.onclick = console.log('click');
+    console.log(observationsBtn);
+  }
 };
 
 
 /**
  * Function to begin the exit interview 
+ * TODO
  */
 const beginInterview = function() {
   // Update buttons and onclicks
