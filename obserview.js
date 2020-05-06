f9533e524dd668ba9950c0e0c013ebe3d25a675e
Sam Talpey <47189754+SamTalpey@users.noreply.github.com>
2020-05-05 21:32:17 -0400

    Floorplan functionality (#1)
    * remove custom stylesheet
    
    * Removing node app
    
    * Adding coordinate marker functionality
    
    * Call function
    
    * correct image dimensions
    
    * removing toggleSheet for testing
    
    * '' necessary
    
    * Addind D3 for vis
    
    * SVG changes and pages test
    
    * Testing image scale with markers
    
    * First attempt at display selection via markers
    
    * Various fixes for display selection
    
    * adding px
    
    * Add indicator for currently selected display, sensitivity may need tweaks
    
    * Cleanup for master

--- obserview.js
+++ obserview.js
@@ -2,24 +2,17 @@
 // =============================================
 
 // Variables for use
-// var observiewSheet = document.getElementById('obserview-sheet');
-// var clearBtn = document.getElementById('obserview-clearbtn');
-// var submitBtn = document.getElementById('obserview-submitbtn');
-// var observationsBtn = document.getElementById('obserview-observationsbtn');
-// var interviewBtn = document.getElementById('obserview-interviewbtn');
+const localStorage = window.localStorage,
+      coordFileName = 'room3CoordsClean.csv',
+      imageFileName = 'room3FloorPlan.png',
+      imageScale = 0.5,
+      markerRadius = 10;
 
 // Generate page
 window.onload = function() {
   console.log('Initializing page');
   toggleSheet(0);
-};
-
-/**
- * Function to initialize observation sheet
- * May not be necessary due to toggleSheet(isLive)
- */
-const initSheet = function() {
-
+  loadImage();
 };
 
 /**
@@ -27,7 +20,6 @@ const initSheet = function() {
  * Call with true for live observations, and false for init observations
  */
 const toggleSheet = function(isLive) {
-  console.log('Toggling sheet');
   let observiewSheet = document.getElementById('obserview-sheet');
   observiewSheet.innerHTML = '';
   let clearBtn = document.getElementById('obserview-clearbtn');
@@ -39,7 +31,7 @@ const toggleSheet = function(isLive) {
     // Create clock/stopwatch
     // TODO
 
-    // Create element to show current exhibit
+    // Create element to indicate current display
     // TODO
 
     // Creating live observation submission form 
@@ -69,11 +61,11 @@ const toggleSheet = function(isLive) {
     other.id = 'obserview-sheet-other';
 
     // Appending init observation elements
-    observiewSheet.innerHTML = 'Demographic: ';
+    observiewSheet.innerHTML = 'Demographic Notes: ';
     observiewSheet.append(demographic);
-    observiewSheet.innerHTML += '<br><br>Size: ';
+    observiewSheet.innerHTML += '<hr>Size: ';
     observiewSheet.append(size);
-    observiewSheet.innerHTML += '<br><br>Other Notes: ';
+    observiewSheet.innerHTML += '<hr>Other Notes: ';
     observiewSheet.append(other);
 
     // Editing button onclicks/text
@@ -85,6 +77,84 @@ const toggleSheet = function(isLive) {
   }
 };
 
+
+/**
+ * Function to load floor plan image and marking
+ * TODO make images selectable
+ */
+const loadImage = function() {
+  // Creating SVG for image to be placed in
+  let width = 980;
+  let height = 1066;
+  var svg = d3.select(document.getElementById('floorplan-image')).append('svg')
+    .attr('width', width * imageScale).attr('height', height * imageScale);
+
+  // Appending image
+  svg.append('image')
+    .attr('xlink:href', imageFileName)
+    .attr('width', width * imageScale)
+    .attr('height', height * imageScale)
+
+  // Adding marker on click
+  svg.on('click', function() {
+    console.log('Adding marker to floorplan');
+    let coords = d3.mouse(this);
+
+    // Clear prev marker and add new one
+    d3.selectAll('#dot')
+      .remove()
+    svg.append('circle')
+      .attr('id', 'dot')
+      .attr('cx', coords[0])
+      .attr('cy', coords[1])
+      .attr('r', markerRadius + 'px')
+      .attr('fill', 'none')
+      .attr('stroke', 'red')
+      .attr('stroke-width', 1)
+    
+    // Use location to find display
+    getLocation(coords[0], coords[1])
+  })
+};
+
+/**
+ * Function to find the closest display to a marked location
+ * Saves to window's local storage
+ */
+const getLocation = function(x, y) {
+  let maxDistance = 15;
+  var result;
+
+  // Unscaled pixel coords
+  let xPx = x / imageScale;
+  let yPx = y / imageScale;
+
+  // Check each of the display coords from csv
+  d3.csv(coordFileName).then(coords => {
+    // Find display closest to marker
+    for(let i = 0; i < coords.length; i++) {
+      let dx = coords[i].x - xPx;
+      let dy = coords[i].y - yPx;
+      let distance = Math.sqrt((dx ** 2) + (dy ** 2));
+
+      // Check if match is closest so far
+      if(distance < maxDistance) {
+        maxDistance = distance;
+
+        // Check if display is within marker
+        let left = (dx ** 2) + (dy ** 2);
+        let right = (markerRadius * 2) ** 2;
+        if(left < right) {
+          console.log('Match found: Display', coords[i].display)
+          result = coords[i].display
+        }
+      }
+    }
+    // Save the result
+    console.log('Closest match:', result)
+    document.getElementById('currentDisplay').innerText = 'Current Display: ' + (result ? result : 'None');
+  })
+};
 
 /**
  * Function to begin the exit interview 
